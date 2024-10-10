cf0e65eb4238777b612dec69a9c7ccfdb97289ce
V1acl <v1aclw@gmail.com>
2024-10-10 20:17:22 +0300

    New project one command to install

--- docker/php/dev.sh
+++ docker/php/dev.sh
@@ -1,8 +1,8 @@
+composer install --classmap-authoritative
+
 bin/console doctrine:database:create --no-interaction --if-not-exists
 bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 bin/console importmap:install
 bin/console asset-map:compile
-
-chown -R www-data: var
 
 php-fpm -F
